d643d47e2570681c9edbf12fd7915076ced65d77
Claus Peter <34019306+clausaxon@users.noreply.github.com>
2019-02-22 16:43:52 +0700

    Add files via upload

---
+++ soal3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+randompas=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 12 )
+n=1
+flag=0
+while [ $flag==0 ]
+  do
+  if [ $(find /home/claus/Desktop/ -name password$n.txt | wc -l) -gt 0 ]
+   then
+    flag=0
+    let n=$n+1
+  else
+   echo $randompas > /home/claus/Desktop/password$n.txt
+   break
+fi
+done
